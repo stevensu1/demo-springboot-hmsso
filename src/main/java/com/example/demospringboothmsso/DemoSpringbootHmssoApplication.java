9bbf44972f66525448e4d2d6746767662b6ea9de
sue <1934192188@qq.com>
2024-05-14 22:09:37 +0900

    Initial commit

---
+++ src/main/java/com/example/demospringboothmsso/DemoSpringbootHmssoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demospringboothmsso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootHmssoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DemoSpringbootHmssoApplication.class, args);
+    }
+
+}
